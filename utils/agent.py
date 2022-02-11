0043c1d7aa654cdfc1ce3b7fe83785f1876c75be
ARTushar <1605070@ugrad.cse.buet.ac.bd>
2022-02-11 17:45:58 +0600

    add bot agent

--- utils/agent.py
+++ utils/agent.py
@@ -1,31 +1,16 @@
-from enum import Enum, auto
-
+from subprocess import Popen, PIPE
+from environments.lines_of_action.lac import LACEnv
 import gym
 import numpy as np
 
-from environments.lines_of_action.lac import LACEnv
 
-
-class AgentType(Enum):
-    RANDOM = auto()
-    MODEL = auto()
-
-
-class Agent:
-    def __init__(self, agent_type: AgentType, model=None):
-        self.agent_type = agent_type
-        self.model = model
+class RandomAgent:
+    def __init__(self, player_no):
+        self.player_no = player_no
 
     def choose_action(self, env: gym.Env, choose_best_action: bool):
-        action_probs = None
-        if self.agent_type == AgentType.RANDOM:
-            action_probs = np.array(env.get_random_action())
-
-        # TODO: Add model support
-
+        action_probs = np.array(env.get_random_action())
         action = np.argmax(action_probs)
-
-        assert action_probs is not None
         if not choose_best_action:
             action = self.sample_action(action_probs)
 
@@ -37,8 +22,50 @@ class Agent:
         return action
 
 
-def test_agent():
-    agents = [Agent(AgentType.RANDOM), Agent(AgentType.RANDOM)]
+class BotAgent:
+    def __init__(self, filename, player_no):
+        self.filename = filename,
+        self.player_no = player_no
+        self.process = self.open_minmax_bot(filename, player_no)
+
+    def get_move(self, board):
+        board_str = self.convert_board(board)
+        print(board_str, file=self.process.stdin, flush=True)
+        moves = self.process.stdout.readline()
+        moves = [int(x) for x in moves[:-1].split()]
+        return moves
+
+    @staticmethod
+    def convert_board(board):
+        board_str = ''
+        for row in board:
+            for val in row:
+                board_str += BotAgent.get_first_char(val) + ' '
+            board_str += '\n'
+        return board_str
+
+    @staticmethod
+    def get_first_char(val):
+        if val == -1:
+            return 'b'
+        elif val == 1:
+            return 'r'
+        return 'e'
+
+    @staticmethod
+    def open_minmax_bot(filename, player_no, board_size=8):
+        if player_no == 1:
+            player_type = 'b'
+        else:
+            player_type = 'r'
+        command = [filename, player_type, str(board_size)]
+        return Popen(command, stdin=PIPE, stdout=PIPE, universal_newlines=True, bufsize=1)
+
+# TODO: Create a model agent
+
+
+def test_random_agent():
+    agents = [RandomAgent(1), RandomAgent(2)]
     env = LACEnv()
 
     env.render()
@@ -54,7 +81,24 @@ def test_agent():
                 break
 
 
+def test_bot_agent():
+    agents = [BotAgent('../bots/player1', 1), BotAgent('../bots/player2', 2)]
+    env = LACEnv()
+    env.render()
+    game_done = False
+    while not game_done:
+        for agent in agents:
+            moves = agent.get_move(env.engine.board)
+            print('move: ', moves)
+            action = env.move_to_action(((moves[0], moves[1]), (moves[2], moves[3])))
+            _, reward, done, _ = env.step(action)
+            env.render()
+            if done:
+                game_done = True
+                break
+
+
 if __name__ == '__main__':
     np.random.seed(1)
-    test_agent()
-
+    # test_random_agent()
+    test_bot_agent()
