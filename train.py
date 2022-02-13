fa46cf6c571e287fbe413f6bf1362236ecb18403
AR Tushar <1605070@ugrad.cse.buet.ac.bd>
2022-02-13 08:07:43 +0000

    add resume training from saved model

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from utils.selfplay import SelfPlayEnv, OpponentType
 def train_with_random():
     policy_kwargs = dict(
         features_extractor_class=CustomCNN,
-        features_extractor_kwargs=dict(features_dim=64*64),
+        features_extractor_kwargs=dict(features_dim=64*64*2),
 
     )
     env = SelfPlayEnv(opponent_type=OpponentType.RANDOM, verbose=0)
@@ -44,8 +44,27 @@ def evaluate_trained_model():
     print("mean reward : ", mean_reward)
 
 
+def continue_train():
+    env = SelfPlayEnv(opponent_type=OpponentType.RANDOM, verbose=0)
+    best_mode_name = 'best_model.zip'
+    path = os.path.join(config.TMPMODELDIR, best_mode_name)
+    model = PPO.load(path, env=env)
+    eval_callback = EvalCallback(
+        eval_env=Monitor(SelfPlayEnv(opponent_type=OpponentType.RANDOM)),
+        eval_freq=1000,
+        best_model_save_path=config.TMPMODELDIR,
+        log_path=config.LOGDIR,
+        deterministic=True,
+        render=False
+    )
+    model.learn(total_timesteps=int(1e9), callback=[eval_callback], tb_log_name='tb')
+
+
+
 if __name__ == '__main__':
-    train_with_random()
+    # train_with_random()
     # evaluate_trained_model()
+    continue_train()
+
 
 
